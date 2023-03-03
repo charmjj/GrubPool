4038a2ecc4f187867fed1c1388dbadafea15cb67
charmjj <jjlim.2019@sis.smu.edu.sg>
2023-03-04 01:19:55 +0800

    project initialisation

---
+++ app/src/main/java/cs461/g2t10/grubpool/MainActivity.kt
@@ -0,0 +1,11 @@
+package cs461.g2t10.grubpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
