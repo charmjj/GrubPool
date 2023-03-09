9068c69febf76f85c67f85337af5c10e2a6402e8
linsyhen99 <linsyhen99@gmail.com>
2023-03-10 01:02:13 +0800

    Scaffolding basic camera widget access permissions and included google ML kit library for OCR model access
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ app/src/main/java/cs461/g2t10/grubpool/OcrActivity.kt
@@ -0,0 +1,109 @@
+package cs461.g2t10.grubpool
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.util.logging.Level
+import java.util.logging.Logger
+
+class OcrActivity : AppCompatActivity() {
+    private var logger = Logger.getLogger("OcrActivity")
+
+    // Variables associated with OCR library
+    private var recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    // Variables associated with Camera widget
+    private val cameraRequest = 1
+    lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        logger.level = Level.ALL
+        setContentView(R.layout.activity_ocr)
+
+        if (
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.CAMERA), cameraRequest
+            )
+        } else {
+            dispatchTakePictureIntent()
+        }
+
+        imageView = findViewById(R.id.cameraWidget)
+        val captureButton: Button = findViewById(R.id.captureButton)
+        captureButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == cameraRequest) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest && resultCode == RESULT_OK) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
+
+            extractTextFromImage(photo)
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, cameraRequest)
+    }
+
+    fun extractTextFromImage(bitmap: Bitmap) {
+        logger.info("Extracting text from image...")
+        val image = InputImage.fromBitmap(bitmap, 0)
+        recognizer.process(image).addOnSuccessListener { visionText ->
+            val resultText = visionText.text
+            logger.info("This is the text generated from the image: $resultText")
+            for (block in visionText.textBlocks) {
+                val blockText = block.text
+                val blockCornerPoints = block.cornerPoints
+                val blockFrame = block.boundingBox
+                for (line in block.lines) {
+                    val lineText = line.text
+                    val lineCornerPoints = line.cornerPoints
+                    val lineFrame = line.boundingBox
+                    for (element in line.elements) {
+                        val elementText = element.text
+                        val elementCornerPoints = element.cornerPoints
+                        val elementFrame = element.boundingBox
+                    }
+                }
+            }
+        }.addOnFailureListener { e ->
+            logger.log(Level.SEVERE, "An error occurred while processing the image", e)
+            Toast.makeText(this, "Failed to recognize text from image", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
