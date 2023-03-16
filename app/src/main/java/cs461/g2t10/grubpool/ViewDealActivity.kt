6d64e27100352b344f19d227b48a042e669e1121
Lim Zhi Hao <70679812+pandaponn@users.noreply.github.com>
2023-03-17 00:27:12 +0800

    food deal UI

---
+++ app/src/main/java/cs461/g2t10/grubpool/ViewDealActivity.kt
@@ -0,0 +1,41 @@
+package cs461.g2t10.grubpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class ViewDealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_deal)
+
+        // Receives an Intent with item ID
+
+        // Query DB for item information by item ID
+
+        // Insert item information into View
+        var dealImage = findViewById<ImageView>(R.id.dealImage)
+        var itemLocation = findViewById<TextView>(R.id.itemLocation)
+        var itemShopName = findViewById<TextView>(R.id.itemShopName)
+        var itemName = findViewById<TextView>(R.id.itemName)
+        var itemDesc = findViewById<TextView>(R.id.itemDesc)
+        var itemPrice = findViewById<TextView>(R.id.itemPrice)
+        var itemDiscount = findViewById<TextView>(R.id.itemDiscount)
+
+        dealImage.setImageResource(R.drawable.rounded_corner)
+        itemLocation.text = ""
+        itemShopName.text = ""
+        itemName.text = ""
+        itemDesc.text = ""
+        itemPrice.text = ""
+        itemDiscount.text = ""
+
+    }
+
+    // Checkout Food Deal
+    fun checkout(view: View) {
+
+    }
+}
