6d18342330511d030481f93f34b82ab4e95de974
linsyhen99 <linsyhen99@gmail.com>
2023-03-18 11:15:43 +0800

    Splitting up the image capture activity and the ocr activity
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ app/src/main/java/cs461/g2t10/grubpool/ImageCaptureActivity.kt
@@ -0,0 +1,11 @@
+package cs461.g2t10.grubpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageCaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_capture)
+    }
+}
