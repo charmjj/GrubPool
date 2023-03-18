5fc01fc6344723e507b287ec727816b4559bd07b
linsyhen99 <linsyhen99@gmail.com>
2023-03-18 12:48:25 +0800

    Setting up the image capture intent and writing the image file to external storage for persistent
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- app/src/main/java/cs461/g2t10/grubpool/ImageCaptureActivity.kt
+++ app/src/main/java/cs461/g2t10/grubpool/ImageCaptureActivity.kt
@@ -1,11 +1,97 @@
 package cs461.g2t10.grubpool
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.Level
+import java.util.logging.Logger
 
 class ImageCaptureActivity : AppCompatActivity() {
+    private var logger = Logger.getLogger("ImageCaptureActivity")
+    private val cameraRequest = 100
+    lateinit var imageView: ImageView
+    lateinit var currentPhotoPath: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logger.level = Level.ALL
         setContentView(R.layout.activity_image_capture)
+
+        imageView = findViewById(R.id.imageDisplay)
+        val snapButton = findViewById<Button>(R.id.snapButton)
+        snapButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                applicationContext, android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.CAMERA), cameraRequest
+            )
+        } else {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+
+            // TODO: Start another activity to process the image through ocr
+        } else {
+            Toast.makeText(
+                this,
+                "Permission denied for camera access, you can allow it in the settings",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    logger.warning("An exception happened while creating your filename $ex")
+                    null
+                }
+                photoFile?.also {
+                    val photoUri: Uri =
+                        FileProvider.getUriForFile(this, "com.example.android.fileprovider", it)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(takePictureIntent, cameraRequest)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile("DEAL_${timestamp}_", ".jpg", storageDir)
+            .apply { currentPhotoPath = absolutePath }
     }
 }
