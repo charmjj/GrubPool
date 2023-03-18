c53497ca592500ced850da12ae3de75747171fc9
linsyhen99 <linsyhen99@gmail.com>
2023-03-18 15:24:19 +0800

    Refined the image capture model and improved user flow to allow backtracking
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- app/src/main/java/cs461/g2t10/grubpool/ImageCaptureActivity.kt
+++ app/src/main/java/cs461/g2t10/grubpool/ImageCaptureActivity.kt
@@ -1,21 +1,19 @@
 package cs461.g2t10.grubpool
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import java.io.File
-import java.io.IOException
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.logging.Level
@@ -24,8 +22,11 @@ import java.util.logging.Logger
 class ImageCaptureActivity : AppCompatActivity() {
     private var logger = Logger.getLogger("ImageCaptureActivity")
     private val cameraRequest = 100
-    lateinit var imageView: ImageView
+    private lateinit var imageView: ImageView
+    lateinit var imageBitmap: Bitmap
     lateinit var currentPhotoPath: String
+    lateinit var captureImageButton: Button
+    lateinit var continueButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,31 +34,38 @@ class ImageCaptureActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image_capture)
 
         imageView = findViewById(R.id.imageDisplay)
-        val snapButton = findViewById<Button>(R.id.snapButton)
-        snapButton.setOnClickListener {
+        continueButton = findViewById(R.id.continueButton)
+        continueButton.isEnabled = false
+
+        captureImageButton = findViewById(R.id.snapButton)
+        captureImageButton.setOnClickListener {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraIntent, cameraRequest)
         }
 
         if (ContextCompat.checkSelfPermission(
                 applicationContext, android.Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_DENIED
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, cameraRequest)
+        } else {
             ActivityCompat.requestPermissions(
                 this, arrayOf(android.Manifest.permission.CAMERA), cameraRequest
             )
-        } else {
-            dispatchTakePictureIntent()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == cameraRequest && resultCode == RESULT_OK) {
-            val imageBitmap = data!!.extras!!.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
+            var capturedImageBitmap = data!!.extras!!.get("data") as Bitmap
+            capturedImageBitmap = increaseImageResolution(capturedImageBitmap)
+            imageView.setImageBitmap(capturedImageBitmap)
+            imageBitmap = capturedImageBitmap
 
-            // TODO: Start another activity to process the image through ocr
+            captureImageButton.text = "Retake"
+            continueButton.isEnabled = true
         } else {
             Toast.makeText(
                 this,
@@ -67,31 +75,44 @@ class ImageCaptureActivity : AppCompatActivity() {
         }
     }
 
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    logger.warning("An exception happened while creating your filename $ex")
-                    null
-                }
-                photoFile?.also {
-                    val photoUri: Uri =
-                        FileProvider.getUriForFile(this, "com.example.android.fileprovider", it)
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-                    startActivityForResult(takePictureIntent, cameraRequest)
-                }
-            }
+    fun onContinue(view: View) {
+        val ocrIntent = Intent(this, OcrActivity::class.java)
+        generateFilename()
+
+        val stream: FileOutputStream = this.openFileOutput(currentPhotoPath, Context.MODE_PRIVATE)
+        val bitmap: Bitmap = imageBitmap
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        stream.close()
+
+        ocrIntent.putExtra("imageFilePath", currentPhotoPath)
+        startActivity(ocrIntent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == cameraRequest && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        } else {
+            Toast.makeText(
+                this,
+                "Permission denied for camera access, you can allow it in the settings",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
+    private fun generateFilename() {
         // Create an image file name
         val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-        return File.createTempFile("DEAL_${timestamp}_", ".jpg", storageDir)
-            .apply { currentPhotoPath = absolutePath }
+        currentPhotoPath = "YUMMERZ_${timestamp}_.png"
+    }
+
+    private fun increaseImageResolution(image: Bitmap): Bitmap {
+        val width = image.width * 2
+        val height = image.height * 2
+        return Bitmap.createScaledBitmap(image, width, height, true)
     }
 }
