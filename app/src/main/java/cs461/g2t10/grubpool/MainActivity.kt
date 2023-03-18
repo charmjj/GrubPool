af0d58d69de091cc3f0e5adadd7f550fc42b9ef2
linsyhen99 <linsyhen99@gmail.com>
2023-03-18 13:28:38 +0800

    Added intent bindings to the shareGrub button
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- app/src/main/java/cs461/g2t10/grubpool/MainActivity.kt
+++ app/src/main/java/cs461/g2t10/grubpool/MainActivity.kt
@@ -1,11 +1,18 @@
 package cs461.g2t10.grubpool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun shareGrub() {
+        val shareGroupIntent = Intent(this, ImageCaptureActivity::class.java)
+        startActivity(shareGroupIntent)
     }
 }
