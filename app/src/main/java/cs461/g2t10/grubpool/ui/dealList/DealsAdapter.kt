b2d0b0f1cecb5ba3b877506cc77af2a6d733dc5c
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2023-04-02 12:08:17 +0800

    apply formatting and layout changes
    + fixed incorrect date format displayed
    + added '$' sign for cost
    + layout enhancements for edit listing dialogue

--- app/src/main/java/cs461/g2t10/grubpool/ui/dealList/DealsAdapter.kt
+++ app/src/main/java/cs461/g2t10/grubpool/ui/dealList/DealsAdapter.kt
@@ -9,6 +9,8 @@ import cs461.g2t10.grubpool.data.api.S3_BASE_URL
 import cs461.g2t10.grubpool.data.models.FoodDeal
 import cs461.g2t10.grubpool.databinding.DealsItemBinding
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 class DealsAdapter : RecyclerView.Adapter<DealsAdapter.DealViewHolder>() {
@@ -48,8 +50,8 @@ class DealsAdapter : RecyclerView.Adapter<DealsAdapter.DealViewHolder>() {
             val context = root.context
             dealName.text = data.name
             dealDescription.text = data.description
-            finalPrice.text = context.getString(R.string.dollar_value, ((100 - data.discount) * data.price) / 100)
-            originalPrice.text = context.getString(R.string.dollar_value, data.price)
+            finalPrice.text = "$" + context.getString(R.string.dollar_value, ((100 - data.discount) * data.price) / 100)
+            originalPrice.text = "$" + context.getString(R.string.dollar_value, data.price)
             discount.text = context.getString(R.string.percent_value, data.discount, "%")
             dealTime.text = data.date?.format()
             Glide.with(context).load(S3_BASE_URL.plus(data.imageUrl)).into(cartImage)
@@ -67,8 +69,15 @@ class DealsAdapter : RecyclerView.Adapter<DealsAdapter.DealViewHolder>() {
 
 fun Date?.format(): String {
     return try {
-        val sdf = SimpleDateFormat("DD/MM/YYYY")
-        sdf.format(this)
+//        val sdf = SimpleDateFormat("DD/MM/YYYY")
+//        sdf.format(this)
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+        val stringUnformatted = sdf.format(this)
+//        return sdf.format(date)
+        val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val date = LocalDateTime.parse(stringUnformatted, inputFormatter)
+        return date.format(outputFormatter)
     } catch (_: Exception) {
         ""
     }
